0674f246203b40f8054c0994494e6ba08f37b953
FezVrasta <federico.zivolo@gmail.com>
2016-04-27 21:33:24 +0200

    fixed path

--- webpack.config.deploy.js
+++ webpack.config.deploy.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
-    publicPath: '/netflix'
+    publicPath: '/netflix/'
   },
   module: {
     loaders: [
